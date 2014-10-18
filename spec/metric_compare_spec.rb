52ebe81ee3dd7b818051bb35e6bb1da1c11d6c6e
Jon Oliver <jon@joliver.net>
2014-10-17 20:13:43 -0400

    Remove old tests

--- spec/metric_compare_spec.rb
+++ spec/metric_compare_spec.rb
@@ -5,22 +5,3 @@ describe 'Init' do
     MetricCompare::Init({}, ['report', 'custom_report'])
   end
 end
-
-describe 'FlogComparer' do
-  it 'makes a comparison' do
-    comparison = MetricCompare::FlogComparer.new('report.yml', 'custom_report.yml')
-    comparison.render_result
-  end
-end
-describe 'ReekComparer' do
-  it 'makes a comparison' do
-    comparison = MetricCompare::ReekComparer.new('report.yml', 'custom_report.yml')
-    comparison.render_result
-  end
-end
-describe 'BestPracticeComparer' do
-  it 'makes a comparison' do
-    comparison = MetricCompare::BestPracticeComparer.new('report.yml', 'custom_report.yml')
-    comparison.render_result
-  end
-end
